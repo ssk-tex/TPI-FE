0b1ab7b3a1d2d8755775488fbd35014453526112
Avinash jha <148786597+ssk-tex@users.noreply.github.com>
2026-04-21 18:21:15 +0530

    live env set

--- src/environment/env.js
+++ src/environment/env.js
@@ -11,12 +11,12 @@ const LSB = "http://127.0.0.1:2031/api/v1";
 const dotnetLSB = "http://localhost:5152/api/v1";
 
 // -- uat --
-const PSB = "http://train-ifms.wb.gov.in/wbjit_integration_tpi_utils/v1";
-// const dotnetPSB = "http://train-ifms.wb.gov.in/wbjitintegration-uat/api/v1";
-const dotnetPSB = "https://ifms.wb.gov.in/wbjitintegration-live/api/v1";
+const PSB = "https://train-ifms.wb.gov.in/wbjit_integration_tpi_utils/v1"; 
+const dotnetPSB = "http://train-ifms.wb.gov.in/wbjitintegration-uat/api/v1";
+// const dotnetPSB = "https://ifms.wb.gov.in/wbjitintegration-live/api/v1";
 // const dotnetPSB = "";
 
-export const isDev = true;
+export const isDev = false;
 
 export const URL = isDev ? LSB : PSB;
 export const dotnetURL = isDev ? dotnetLSB : dotnetPSB;
